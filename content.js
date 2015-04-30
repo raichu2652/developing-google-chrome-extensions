26ebcfb781706b3f89b54347a1bd13854a943f6a
Paul Tu <raichu2652@gmail.com>
2015-05-01 02:10:30 +0800

    Permission modified
    manifest.json
    "permissions", "http://*/" added.

--- content.js
+++ content.js
@@ -2,6 +2,8 @@ chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
 	switch(message.type) {
 		case "colors-div":
 			var divs = document.querySelectorAll("div");
+
+			console.log(divs);
 			if(divs.length === 0) {
 				alert("There are no any divs in the page.");
 			} else {
